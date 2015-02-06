c73463fe19d2d2be0c617a27079b33a69828d5cc
yashchitalia <yashc@yashc.fake>
2015-02-06 01:42:33 -0500

    Added arp scanning functionality to Sentinel. Needs testing on the pi

--- sentinel.py
+++ sentinel.py
@@ -23,6 +23,7 @@ import datetime
 import smtplib
 import ConfigParser
 import pickle as pkl
+import subprocess as sp
 # Here are the email package modules we'll need
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
@@ -39,25 +40,40 @@ username = senders_email
 password = config.get('Email_configure', 'password')
 archive_directory = config.get('General_configure', 'archive_dir')
 pics_directory = config.get('General_configure', 'motion_dir')
-
+ip_address = config.get('Network', 'ip_address')
 previous_intrusion_datetime = pkl.load(open("last_intrusion.p", "rb" ))
 current_intrusion_datetime = datetime.datetime.now()
 intrusion_time_delta = current_intrusion_datetime - previous_intrusion_datetime 
 MAX_PICS_BEFORE_EMAIL = 5
 
+
+def owner_at_home():
+    '''Performs an arp-scan and returns a string. We will scan the string
+    and search for our network address'''
+    networks = sp.check_output(['sudo', 'arp-scan' ,'-interface', 
+                         'en1', '--localnet'])
+    if ip_address in networks:
+        return True
+    else
+        return False
+
+
 def email_sender():
-    ''' Counts the number of pictures in the /tmp/motion' folder and if the number of pics
-    is  geq 5, then we first send an email to the user with those pics attached, and we
-    will move those pics to an archive folder, with the date and time of intrusion. We will
-    also store the current date and time in a pickle file, and will click more pics only if 
+    ''' Counts the number of pictures in the /tmp/motion' folder and if the 
+    number of pics is  geq 5, then we first send an email to the user with 
+    those pics attached, and we will move those pics to an archive folder, 
+    with the date and time of intrusion. We will also store the current date 
+    and time in a pickle file, and will click more pics only if 
     current time is greater than 5 mins of the last intrusion'''
 
     pics = [f for f in os.listdir(pics_directory) if f.endswith('.jpg')]
     image_count = len(pics)
-    if image_count >= MAX_PICS_BEFORE_EMAIL and intrusion_time_delta >= datetime.timedelta(minutes=1):
+    if image_count >= MAX_PICS_BEFORE_EMAIL and (intrusion_time_delta >= 
+            datetime.timedelta(minutes=1)) and (!owner_at_home()):
         # Create the container (outer) email message.
         msg = MIMEMultipart('mixed')
-        msg['Subject'] = '[SENTINEL MSG]['+str(current_intrusion_datetime)+'] The sentinel has spotted an intruder!'
+        msg['Subject'] = ('[SENTINEL MSG]['+str(current_intrusion_datetime)+']'
+        'The sentinel has spotted an intruder!')
         msg['From'] = senders_email
         msg['To'] = destination_email
         msg.preamble = 'Pics of the intruder:'
@@ -71,7 +87,8 @@ def email_sender():
                 msg.attach(img)
                 print "Image attached to email"
             except:
-                print "[ERROR]: Couldn't attach picture "+p+". Exiting this pass."
+                print ("[ERROR]: Couldn't attach picture "+p+". "
+                "Exiting this pass.")
                 return
         try:
             # Send the email via gmail's SMTP server.
@@ -82,21 +99,26 @@ def email_sender():
                 server.login(username, password)
                 print "Login Successful"
             except:
-                print "[ERROR]: Could not log in. Are your login details correct?"
+                print ("[ERROR]: Could not log in."
+                "Are your login details correct?")
                 return
             server.sendmail(senders_email, [destination_email], msg.as_string())
             server.close()
             print "Email Sent Successfully!"
         except:
-            print "[ERROR] Sendmail function may not be getting the right objects. Email not sent."
+            print ("[ERROR] Sendmail function may not be getting the right ")
+            "objects. Email not sent.")
             return
 
-        pkl.dump(current_intrusion_datetime, open("/home/pi/sentinel/last_intrusion.p", "wb"))
-        #Make a directory in the Motion archives folder with the current date and time name
+        pkl.dump((current_intrusion_datetime, 
+            open("/home/pi/sentinel/last_intrusion.p", "wb")))
+        #Make a directory in the Motion archives folder 
+        #with the current date and time name
         os.mkdir(archive_directory+'/'+str(current_intrusion_datetime))
         #Add the present pics to this archived folder
         for p in pics:
-            os.rename(pics_directory+'/'+p,  archive_directory+'/'+str(current_intrusion_datetime)+'/'+p)
+            os.rename((pics_directory+'/'+p,  
+                archive_directory+'/'+str(current_intrusion_datetime)+'/'+p))
 
     else:
         print "Secretly snapping the intruder."
